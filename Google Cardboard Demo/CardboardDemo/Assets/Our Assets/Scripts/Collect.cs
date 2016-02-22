5adf345e8fd1884db07fca3fafbf26edbfce7202
Adina Shanholtz <adinas@microsoft.com>
2016-02-22 16:48:17 -0500

    Committing Google Cardboard Demo
    Committing Google Cardboard demo by Adina Shanholtz

---
+++ Google Cardboard Demo/CardboardDemo/Assets/Our Assets/Scripts/Collect.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+
+public class Collect : MonoBehaviour
+{
+
+    //Create game controller
+    private GameController gameController;
+
+    /*Our Start() function finds the instance of our GameController and by default sets our 
+    collectable so that we're not looking at it*/
+    void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+        SetGazedAt(false);
+    }
+
+    /*LateUpdate() will close the app if user presses back button on both Android and iOS*/
+    void LateUpdate()
+    {
+        Cardboard.SDK.UpdateState();
+        if (Cardboard.SDK.BackButtonPressed)
+        {
+            Application.Quit();
+        }
+    }
+
+    /*SetGazedAt() changes the color of the collectable based on whether or not we're looking at it. 
+    Red for not, green for yes.*/
+    public void SetGazedAt(bool gazedAt)
+    {
+        GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
+    }
+
+    /*Collected() increments our score value held by our GameController and deactivates the collectable*/
+    public void Collected()
+    {
+        gameController.AddOne();
+        gameObject.SetActive(false);
+    }
+
+}
+
+
---
+++ Google Cardboard Demo/README.md
@@ -0,0 +1,4 @@
+# CollectThemAllVR
+VR Demo for Google Cardboard by Adina Shanholtz @FeyTechnologist
+
+Detailed instructions for creating this demo can be found here: http://adinashanholtz.com/5-steps-to-making-your-first-vr-game/ 
